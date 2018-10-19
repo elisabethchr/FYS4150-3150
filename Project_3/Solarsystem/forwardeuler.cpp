a3cb03ce9cbdea1971a3a8eef17edfda74047c8f
elisachr <elisachr@student.matnat.uio.no>
2018-10-19 18:12:06 +0200

    It worksssss!!

--- Project_3/Solarsystem/forwardeuler.cpp
+++ Project_3/Solarsystem/forwardeuler.cpp
@@ -5,10 +5,8 @@
 #include <fstream>
 #include <iomanip>
 #include <math.h>
-//#include "WriteToFile.hpp"
 //#include "EnergyTest.hpp"
 #include "forwardeuler.h"
-//#include "initialize.h"
 #include "gravitationalforce.h"
 #include "celestialobject.h"
 #include "solarsystem.h"
@@ -20,77 +18,74 @@ void ForwardEuler::Euler(double dt)// :m_dt(dt)
 }
 
 arma::mat ForwardEuler::Integrate(SolarSystem &input_system){//vec3 position, vec3 velocity, double mass){ // int N, int dim, std::string obj){
-  /*
-  Compute the position of the planet using forward Euler method.
-  */
-  solar = &input_system;
-  int  NumberOfObjects = solar->numberOfObjects();
+    /*
+ * Compute the position of the planet using forward Euler method.
+ */
+    solar = &input_system;
+    int  NumberOfObjects = solar->numberOfObjects();
 
-  int N = 367;
-  int dim = 3;
-  double h = 1.0/((double) 365.25); //1.0/((double) 365.25);
-  arma::mat vel = arma::zeros(dim, N);
-  arma::mat pos = arma::zeros(dim, N);
+    int N = 367*12;
+    int dim = 3;
+    double h = 1.0/((double) 365.25); //1.0/((double) 365.25);
+    arma::mat vel = arma::zeros(dim, N);
+    arma::mat pos = arma::zeros(dim, N);
 
 
-//  Obtain the initial velocities and positions for planets in m_objects;
-  //vec3 posit1, veloc1;
-  //vec3 posit2, veloc2;
+    //Obtain the initial velocities and positions for planets in m_objects;
     std::vector<CelestialObject> &bodies = solar->objects();
-    arma::mat posit = arma::zeros(dim, bodies.size());
-    arma::mat veloc = arma::zeros(dim, bodies.size());
+    arma::mat pos_init = arma::zeros(dim, bodies.size());
+    arma::mat vel_init = arma::zeros(dim, bodies.size());
     arma::vec mass = arma::zeros(bodies.size());
 
     for (int i=0; i<bodies.size(); i++){
-      CelestialObject &obj = bodies[i];
-      for (int j=0; j < dim; j++){
-          veloc(j, i) = obj.velocity(j);
+        CelestialObject &obj = bodies[i];
+        for (int j=0; j < dim; j++){
+            vel_init(j, i) = obj.velocity(j);
         }
-      for(int j=0; j < dim; j++){
-          posit(j, i) = obj.position(j);
-          }
-    mass(i) = obj.mass;
+        for(int j=0; j < dim; j++){
+            pos_init(j, i) = obj.position(j);
+        }
+        mass(i) = obj.mass;
     }
-//std::cout << "veloc =" << veloc << std::endl;
-
-  arma::vec t = arma::zeros(N);
-
-  std::clock_t start, finish;
-  start = std::clock();  // start timing
 
 
+    //Start timing
+    arma::vec t = arma::zeros(N);
+    std::clock_t start, finish;
+    start = std::clock();
 
-//forward euler
+    /*
+ * Forward Euler calculation
+ */
 
-  for(int k=0; k<NumberOfObjects; k++){
-//initializing to position and velociy matrices used in forward euler
-    vel(0, 0) = veloc(0, k); vel(1, 0) = veloc(1, k); vel(2, 0) = veloc(2, k);
-    pos(0, 0) = posit(0, k); pos(1, 0) = posit(1, k); pos(2, 0) = posit(2, k);
-    std::cout << "acceleration for each iteration for each object" << std::endl;
+    for(int k=0; k<NumberOfObjects; k++){
+        //initializing to position and velociy matrices used in forward euler
+        vel(0, 0) = vel_init(0, k); vel(1, 0) = vel_init(1, k); vel(2, 0) = vel_init(2, k);
+        pos(0, 0) = pos_init(0, k); pos(1, 0) = pos_init(1, k); pos(2, 0) = pos_init(2, k);
         for(int i=0; i<N-1; i++){
+            bodies[k].resetForce();
             solar->calculateForcesAndEnergy();
+            bodies[k].position += h*bodies[k].velocity;
+            bodies[k].velocity += h*bodies[k].force/bodies[k].mass;
+
             for (int j=0; j<dim; j++){
-//            std::cout << bodies[k].force << std::endl;
-                vec3 acc = bodies[k].force/bodies[k].mass;
-                vel(j, i+1) = vel(j, i) + h*acc(j);
-                pos(j, i+1) = pos(j, i) + h*vel(j,i);
-                bodies[k].resetForce();
-             }
-          std::string filename = "euler_pos";
-          std::string arg = std::to_string(k);
-          filename.append(arg);
-          filename.append(".txt");
-          WriteToFile write;
-          write.WritetoFile(filename, pos);
-         }
-}
+                pos(j, i+1) = bodies[k].position(j);
+            }
+        }
 
+        //write positions of each object/planet k to file
+        std::string filename = "euler_pos";
+        std::string arg = std::to_string(k);
+        filename.append(arg);
+        filename.append(".txt");
+        WriteToFile write;
+        write.WritetoFile(filename, pos);
+        std::cout << "size of pos for k = " << k << " is " << sizeof(pos) << std::endl;
+    }
 
-  finish = std::clock();   // end timing
-  double time_used = (double)(finish - start)/(CLOCKS_PER_SEC );
-  std::cout << std::setprecision(10) << "Time used: " << time_used << " s at " << N/365 <<" yr" << std::endl;
-  std::cout << "Write positions to file" << std::endl;
-//  getPos(pos);
-//  getVel(vel);
-  return pos;
+    //stop timing
+    finish = std::clock();   // end timing
+    double time_used = (double)(finish - start)/(CLOCKS_PER_SEC );
+    //  std::cout << std::setprecision(10) << "Time used: " << time_used << " s at " << N/365 <<" yr" << std::endl;
+    return pos;
 }
--- Project_3/Solarsystem/solarsystem.cpp
+++ Project_3/Solarsystem/solarsystem.cpp
@@ -14,48 +14,44 @@ SolarSystem::SolarSystem(std::string positionfile, std::string velocityfile, std
     m_kineticEnergy(0),
     m_potentialEnergy(0)
 {
-  Readfile_test readfile;
-  arma::mat pos0 = readfile.Readfile_(positionfile);    // AU
-  arma::mat vel0 = readfile.Readfile_(velocityfile);
-  arma::mat masses = readfile.Readfile_(massfile);   // AU/yr
-  //pos0.print("initpos");
-  //vel0.print("initvel");
-  //vec3::vec3(){return zeros()} ---> see vec3.cpp
-  vec3 pos0_CoM; vec3 vel0_CoM;           // Center of mass
-  vec3 pos0_Mercury; vec3 vel0_Mercury;
-  vec3 pos0_Venus; vec3 vel0_Venus;
-  vec3 pos0_Earth; vec3 vel0_Earth;
-  vec3 pos0_Mars; vec3 vel0_Mars;
-  vec3 pos0_Jupiter; vec3 vel0_Jupiter;
-  vec3 pos0_Saturn; vec3 vel0_Saturn;
-  vec3 pos0_Uranus; vec3 vel0_Uranus;
-  vec3 pos0_Neptune; vec3 vel0_Neptune;
-  vec3 pos0_Pluto; vec3 vel0_Pluto;
-  std::cout << "before masses are assigned to each object" << std::endl;
-  double mass_CoM = masses(0, 0), mass_Mercury = masses(1, 0), mass_Venus = masses(2, 0), mass_Earth = masses(3, 0), mass_Mars = masses(4, 0);
-  double mass_Jupiter = masses(5, 0), mass_Saturn= masses(6, 0), mass_Uranus= masses(7, 0), mass_Neptune= masses(8, 0), mass_Pluto= masses(9, 0);
+    Readfile_test readfile;
+    arma::mat pos0 = readfile.Readfile_(positionfile);    // AU
+    arma::mat vel0 = readfile.Readfile_(velocityfile);
+    arma::mat masses = readfile.Readfile_(massfile);   // AU/yr
+    vec3 pos0_CoM; vec3 vel0_CoM;
+    vec3 pos0_Mercury; vec3 vel0_Mercury;
+    vec3 pos0_Venus; vec3 vel0_Venus;
+    vec3 pos0_Earth; vec3 vel0_Earth;
+    vec3 pos0_Mars; vec3 vel0_Mars;
+    vec3 pos0_Jupiter; vec3 vel0_Jupiter;
+    vec3 pos0_Saturn; vec3 vel0_Saturn;
+    vec3 pos0_Uranus; vec3 vel0_Uranus;
+    vec3 pos0_Neptune; vec3 vel0_Neptune;
+    vec3 pos0_Pluto; vec3 vel0_Pluto;
 
-  for (int j=0; j<3;j++){
-  std::cout << "j = " << j << std::endl;
-    pos0_CoM(j) = pos0(0,j); vel0_CoM(j) = vel0(0,j);
-    pos0_Mercury(j) = pos0(1,j); vel0_Mercury(j)=vel0(1,j);
-    pos0_Venus(j) = pos0(2,j); vel0_Venus(j) = vel0(2,j);
-    pos0_Earth(j) = pos0(3,j); vel0_Earth(j) = vel0(3,j);
-    pos0_Mars(j) = pos0(4,j); vel0_Mars(j) = vel0(4,j);
-    pos0_Jupiter(j) = pos0(5,j); vel0_Jupiter(j) = vel0(5,j);
-    pos0_Saturn(j) = pos0(6,j); vel0_Saturn(j) = vel0(6,j);
-    pos0_Uranus(j) = pos0(7,j); vel0_Uranus(j) = vel0(7,j);
-    pos0_Neptune(j) = pos0(8,j); vel0_Neptune(j) = vel0(8,j);
-    pos0_Pluto(j) = pos0(9,j); vel0_Pluto(j) = vel0(9,j);
+    double mass_CoM = masses(0, 0), mass_Mercury = masses(1, 0), mass_Venus = masses(2, 0), mass_Earth = masses(3, 0), mass_Mars = masses(4, 0);
+    double mass_Jupiter = masses(5, 0), mass_Saturn= masses(6, 0), mass_Uranus= masses(7, 0), mass_Neptune= masses(8, 0), mass_Pluto= masses(9, 0);
+
+    for (int j=0; j<3;j++){
+        pos0_CoM(j) = pos0(0,j); vel0_CoM(j) = vel0(0,j);
+        pos0_Mercury(j) = pos0(1,j); vel0_Mercury(j)=vel0(1,j);
+        pos0_Venus(j) = pos0(2,j); vel0_Venus(j) = vel0(2,j);
+        pos0_Earth(j) = pos0(3,j); vel0_Earth(j) = vel0(3,j);
+        pos0_Mars(j) = pos0(4,j); vel0_Mars(j) = vel0(4,j);
+        pos0_Jupiter(j) = pos0(5,j); vel0_Jupiter(j) = vel0(5,j);
+        pos0_Saturn(j) = pos0(6,j); vel0_Saturn(j) = vel0(6,j);
+        pos0_Uranus(j) = pos0(7,j); vel0_Uranus(j) = vel0(7,j);
+        pos0_Neptune(j) = pos0(8,j); vel0_Neptune(j) = vel0(8,j);
+        pos0_Pluto(j) = pos0(9,j); vel0_Pluto(j) = vel0(9,j);
     }
 
-    std::cout << "mass sun" << mass_CoM << std::endl;
-    std::cout << "Constructor SolarSystem okay" << std::endl;
     CelestialObject &sun = createCelestialObject(pos0_CoM, vel0_CoM, mass_CoM);
-//    CelestialObject &mercury = createCelestialObject(pos0_Mercury, vel0_Mercury, mass_Mercury);
-//    CelestialObject &venus = createCelestialObject(pos0_Venus, vel0_Venus, mass_Venus);
     CelestialObject &earth = createCelestialObject(pos0_Earth, vel0_Earth, mass_Earth);
-/*
+    CelestialObject &jupiter = createCelestialObject(pos0_Jupiter, vel0_Jupiter, mass_Jupiter);
+
+    /*
+    CelestialObject &mercury = createCelestialObject(pos0_Mercury, vel0_Mercury, mass_Mercury);
+    CelestialObject &venus = createCelestialObject(pos0_Venus, vel0_Venus, mass_Venus);
     CelestialObject &mars = createCelestialObject(pos0_Mars, vel0_Mars, mass_Mars);
     CelestialObject &jupiter = createCelestialObject(pos0_Jupiter, vel0_Jupiter, mass_Jupiter);
     CelestialObject &saturn = createCelestialObject(pos0_Saturn, vel0_Saturn, mass_Saturn);
@@ -77,32 +73,31 @@ void SolarSystem::calculateForcesAndEnergy()
     m_kineticEnergy = 0;
     m_potentialEnergy = 0;
     m_angularMomentum.zeros();
-/*
+    /*
     for(CelestialObject &object : m_objects) {
         // Reset forces on all bodies
         object.force.zeros();
     }
 */
 
-//GravitationalForce &gravForce;
 
-    for(int i=0; i<numberOfObjects(); i++) {
+    /*
+ * Calculating the gravitationalForce:
+ */
+    for(int i=0; i<numberOfObjects(); i++){
         CelestialObject &object1 = m_objects[i];
         for(int j=i+1; j<numberOfObjects(); j++) {
-            //for (int k=0; k<)
             CelestialObject &object2 = m_objects[j];
-            vec3 dRvec = object1.position - object2.position;
+            vec3 dRvec = object2.position - object1.position;
             double dr = dRvec.length();
-            // Calculate the force and potential energy here
-
-            //Calculating the gravitational force
-            vec3 F1 = G*object1.mass * object2.mass*dRvec/(dr*dr*dr);
+            //            std::cout << "object1_pos = " << object1.position << std::endl;
+            //            std::cout << "object2_pos = " << object2.position << std::endl;
+            std::cout << dr << " " << dRvec << std::endl;
+            vec3 F1 = G*object2.mass*object1.mass*dRvec/(dr*dr*dr);
             vec3 F2 = (-1)*F1;
             object1.addForce(F1);
             object2.addForce(F2);
-
-
-           // gravForce->Gravity(&object1, &object2);
+            //            std::cout << "F1 = " << F1 << std::endl;
         }
 
         m_kineticEnergy += 0.5*object1.mass*object1.velocity.lengthSquared();
@@ -133,30 +128,7 @@ double SolarSystem::kineticEnergy() const
 {
     return m_kineticEnergy;
 }
-/*
-void SolarSystem::writeToFile(string filename)
-{
-    if(!m_file.good()) {
-        m_file.open(filename.c_str(), ofstream::out);
-        if(!m_file.good()) {
-            cout << "Error opening file " << filename << ". Aborting!" << endl;
-            terminate();
-        }
-    }
 
-    m_file << numberOfObjects() << endl;
-//    m_file << "Comment line that needs to be here" << endl;
-    for(CelestialObject &object : m_objects) {
-        m_file << " " << object.position.x() << " " << object.position.y() << " " << object.position.z() << "\n";
-    }
-}
-*/
-/*
-vec3 SolarSystem::angularMomentum() const
-{
-    return m_angularMomentum;
-}
-*/
 std::vector<CelestialObject> &SolarSystem::objects()
 {
     return m_objects;
