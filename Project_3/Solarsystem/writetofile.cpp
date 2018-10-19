4f6cce574ba146b57f6d3c1c0c7d9af576e766ae
elisachr <elisachr@student.matnat.uio.no>
2018-10-19 16:06:52 +0200

    writetofile update

--- Project_3/Solarsystem/writetofile.cpp
+++ Project_3/Solarsystem/writetofile.cpp
@@ -1,6 +1,29 @@
 #include "writetofile.h"
 
-WriteToFile::WriteToFile()
-{
+//WriteToFile::WriteToFile(std::string filename, arma::mat A)
+//{
+int WriteToFile::WritetoFile(std::string filename, arma::mat A){
+    int N = sizeof(A.n_cols);
+    std::cout << "N = " << N << std::endl;
+    int dim = sizeof(A.n_rows);
+    std::cout << "dim = " << dim << std::endl;
+    FILE* myfile = fopen(filename.c_str(), "w");
+    double x; double y;  double z;
+    for(int i=0; i<N-1; i++){
+        for (int j=0; j < dim ; j++){
+            if(j==0){
+            x = A(j,i);
+            }
+            else if(j==1){
+            y = A(j, i);
+            }
+            else if(j==2){
+            z = A(j, i);
+            }
+    fprintf(myfile, "%.8f     %.8f     %.8f\n", x,y,z);
+}
+}
+fclose(myfile);
+return 0;
 
 }
--- Project_3/Solarsystem/writetofile.h
+++ Project_3/Solarsystem/writetofile.h
@@ -1,11 +1,19 @@
 #ifndef WRITETOFILE_H
 #define WRITETOFILE_H
 
+#include <iostream>
+#include <armadillo>
+#include <cmath>
+#include <fstream>
+#include <iomanip>
+#include <math.h>
+#include "vec3.h"
+#include "solarsystem.h"
 
 class WriteToFile
 {
 public:
-    WriteToFile();
+    int WritetoFile(std::string filename, arma::mat A);
 };
 
 #endif // WRITETOFILE_H
