102db42feeed6113f649dc54fe9a321d0fa1e735
elisachr <elisachr@student.matnat.uio.no>
2018-10-20 15:46:02 +0200

    including header fie for including headers

---
+++ Project_3/Solarsystem/include_headers.h
@@ -0,0 +1,12 @@
+#ifndef INCLUDE_HEADERS_H
+#define INCLUDE_HEADERS_H
+
+#include "celestialobject.h"
+#include "forwardeuler.h"
+#include "readfile_test.h"
+#include "solarsystem.h"
+#include "writetofile.h"
+#include "velocityverlet.h"
+#include "vec3.h"
+
+#endif // INCLUDE_HEADERS_H
