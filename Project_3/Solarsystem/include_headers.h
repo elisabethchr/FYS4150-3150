dc9a7f39d2dacd6edeab4ec509756e7b546a20af
elisachr <elisachr@student.matnat.uio.no>
2018-10-20 14:20:51 +0200

    header file including all other headers

---
+++ Project_3/Solarsystem/include_headers.h
@@ -0,0 +1,11 @@
+#ifndef INCLUDE_HEADERS_H
+#define INCLUDE_HEADERS_H
+
+#include "celestialobject.h"
+#include "forwardeuler.h"
+#include "readfile_test.h"
+#include "solarsystem.h"
+#include "writetofile.h"
+#include "vec3.h"
+
+#endif // INCLUDE_HEADERS_H
