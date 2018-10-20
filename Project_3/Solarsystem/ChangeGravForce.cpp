f65b35b5ae0900b54469e78fe57476f6a8b905ed
elisachr <elisachr@student.matnat.uio.no>
2018-10-20 18:55:30 +0200

    changed name of gravitationalforce.cpp

--- Project_3/Solarsystem/ChangeGravForce.cpp
+++ Project_3/Solarsystem/ChangeGravForce.cpp
@@ -27,23 +27,22 @@ double GravitationalForce::Force(double pos, double r)
 }
 
 //alternative way:
-    double GravitationalForce::Gravity(CelestialObject *a, CelestialObject *b)
+    vec3 GravitationalForce::Gravity(CelestialObject &a, CelestialObject &b)
     {
-        object_a = a;
-        object_b = b;
         double pi = acos(-1);
         double G = 4*pi*pi;
 
         vec3 F1;
         vec3 F2;
-        vec3 r_a = object_a -> position;
-        vec3 r_b = object_b -> position;
+        vec3 r_a = a.position;
+        vec3 r_b = b.position;
 
         double distance = (r_b - r_a).length();
 
 //Calculating the gravitational force
-    F1 = G*object_a->mass * object_b->mass*(r_b-r_a)/((double) pow(distance, 3));
+    F1 = G*a.mass * b.mass*(r_b-r_a)/(distance*distance*distance);
     F2 = (-1)*F1;
-    object_a -> addForce(F1);
-    object_b -> addForce(F2);
+    a.addForce(F1);
+    b.addForce(F2);
+    return F1;
     }
