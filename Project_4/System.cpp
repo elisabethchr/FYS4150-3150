9174075de3795f33f49955353166d477fd47ef10
Elisabeth Christensen <42673715+elisabethchr@users.noreply.github.com>
2018-11-02 17:30:03 +0100

    Correct writetofile function

--- Project_4/System.cpp
+++ Project_4/System.cpp
@@ -7,56 +7,81 @@
 #include <armadillo>
 #include <string>
 
-#include "System.h"
-#include "Metropolis.h"
+#include "system.h"
+#include "metropolis.h"
 
 using namespace  std;
 using namespace arma;
 
 // output file
-ofstream ofile;
+
+void System::writefile(int n_spin, int MCs, double Temp, vec expValues, string filename)
+{
+    double norm = 1.0/((double) MCs);
+    double averageE = expValues(0)*norm;
+    double averageE2 = expValues(1)*norm;
+    double averageM = expValues(2)*norm;
+    double averageM2 = expValues(3)*norm;
+    double averageMabs = expValues(4)*norm;
+
+    //double varE = (averageE2 - averageE*averageE)/n_spin/n_spin;
+    //double varM = (averageM2 - averageMabs*averageMabs)/n_spin/n_spin;
+    double Cv = (averageE2 - averageE*averageE)/n_spin/n_spin;
+    double chi = (averageM2 - averageM*averageM)/n_spin/n_spin;
+
+    ofstream ofile;
+    string output = filename;
+    string arg = to_string(n_spin);
+    output.append(arg);
+    output.append(".txt");
+    ofile.open(output, ios::app);
+
+    ofile << setiosflags(ios::showpoint | ios::uppercase);
+    ofile << setw(15) << setprecision(8) << Temp;
+    ofile << setw(15) << setprecision(8) << averageE/n_spin/n_spin;
+    ofile << setw(15) << setprecision(8) << Cv/Temp/Temp;
+    ofile << setw(15) << setprecision(8) << averageM/n_spin/n_spin;
+    ofile << setw(15) << setprecision(8) << chi/Temp;
+    ofile << setw(15) << setprecision(8) << averageMabs/n_spin/n_spin << "\n";
+
+   // ofile.close();
+}
+
+
+void System::initialize(int n_spin, double Temp)
+{
+    /*
+ * Initialize lattice matrices containing spins
+ */
+    spin_matrix.zeros(n_spin,n_spin);
+    for (int y=0; y<n_spin; y++){
+        for (int x=0; x<n_spin; x++){
+            //      if (Temp < 1.5){
+            spin_matrix(y,x) = 1;
+            //      }
+            M += (double) spin_matrix(y,x);
+        }
+    }
+    for (int y=0; y<n_spin; y++){
+        for (int x=0; x<n_spin; x++){
+            E -= (double) (spin_matrix(y,x)*(spin_matrix(periodic(y,n_spin,-1), x) + spin_matrix(y,periodic(x,n_spin,-1))));
+        }
+    }
+}
 
 int System::periodic(int i, int n_spin, int add)
 {
-  return (i+n_spin+add) % (n_spin);
+    return (i+n_spin+add) % (n_spin);       //integer remainder
 }
 
-void System::writefile(int n_spin, int MCs, double Temp, vec average)
-{
-  double norm = 1.0/((double) MCs);
-  double averageE = average(0)*norm;
-  double averageE2 = average(1)*norm;
-  double averageM = average(2)*norm;
-  double averageM2 = average(3)*norm;
-  double averageMabs = average(4)*norm;
-
-  //double varE = (averageE2 - averageE*averageE)/n_spin/n_spin;
-  //double varM = (averageM2 - averageMabs*averageMabs)/n_spin/n_spin;
-  double Cv = (averageE2 - averageE*averageE)/n_spin/n_spin;
-  double chi = (averageM2 - averageM*averageM)/n_spin/n_spin;
-
-  ofile << setiosflags(ios::showpoint | ios::uppercase);
-  ofile << setw(15) << setprecision(8) << Temp;
-  ofile << setw(15) << setprecision(8) << averageE/n_spin/n_spin;
-  ofile << setw(15) << setprecision(8) << Cv/Temp/Temp;
-  ofile << setw(15) << setprecision(8) << averageM/n_spin/n_spin;
-  ofile << setw(15) << setprecision(8) << chi/Temp;
-  ofile << setw(15) << setprecision(8) << averageMabs/n_spin/n_spin << endl;
+arma::mat System::Lattice(){
+return spin_matrix;
 }
 
-void System::initialize(int n_spin, double Temp, mat spin_matrix, double &E, double &M)
-{
-  for (int y=0; y<n_spin; y++){
-    for (int x=0; x<n_spin; x++){
-      if (Temp < 1.5){
-        spin_matrix(y,x) = 1;
-      }
-      M += (double) spin_matrix(y,x);
-    }
-  }
-  for (int y=0; y<n_spin; y++){
-    for (int x=0; x<n_spin; x++){
-      E -= (double) (spin_matrix(y,x)*(spin_matrix(periodic(y,n_spin,-1), x) + spin_matrix(y,periodic(x,n_spin,-1)));
-    }
-  }
+double System::Energy(){
+return E;
+}
+
+double System::MagneticMoment(){
+return M;
 }
