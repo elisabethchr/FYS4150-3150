04112d27965eff126d54361b679b5ec9ce89ff99
Elisabeth Christensen <42673715+elisabethchr@users.noreply.github.com>
2018-11-18 17:19:48 +0100

    Added some comments

--- Project_4/PlotData.py
+++ Project_4/PlotData.py
@@ -2,83 +2,164 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys, glob
 import PlotFunctions as Plot
-
+from scipy import stats
 #filename = 'Ising_nSpin_2_nTemp_1_MC_1000000_.txt'
-filename = sys.argv[1]
-#filename = glob.glob('*.txt')
-data = np.genfromtxt(filename)
-NumberOfValues = data.shape[0]
-NumberOfSpins = int(filename.split('_')[2])
-NumberOfTemp = int(filename.split('_')[4])
-NumberOfMC = int(int(filename.split('_')[6]))#/10)
+plt.figure()
 
-if NumberOfMC >= 10000:
-    steps = int(0.0001*NumberOfMC)
-    NumberOfSteps = 9000
-else:
-    steps = 1
-    NumberOfSteps = NumberOfMC
+Tc = []
+L_inv = []
+# Gather files containing different data results in list
+filenames = []
+for args in range(1,len(sys.argv)):
+    filenames.append(sys.argv[args])
 
-NumberOfMc = int(NumberOfMC/steps)
-#NumberOfMc = int(NumberOfValues/steps)
-#print NumberOfMc
-#NumberOfTemp = int(filename.split('_')[4])
+# Loop over each filename and obtain values
+for file in xrange(len(filenames)):
+    filename = filenames[file]
+    data = np.genfromtxt(filename)
+    NumberOfValues = data.shape[0]
+    NumberOfSpins = int(filename.split('_')[2])
+    NumberOfTemp = int(filename.split('_')[4])
+    NumberOfMC = int(int(filename.split('_')[6]))#/10)
+    spins = []
+    spins.append(NumberOfSpins)
+
+    # Decide number of values per temperature depending on which file size we're looking at
+    if 100000 >= NumberOfMC > 10000:
+        steps = int(0.0001*NumberOfMC)
+        print steps
+        if NumberOfSpins > 20:
+            NumberOfSteps = 900     #If excluding first 10% of data values
+        else:
+            NumberOfSteps = 10000
+    elif NumberOfMC > 100000:
+        if NumberOfSpins >= 20:
+            NumberOfSteps = 9000    #If excluding first 10% of data values
+        else:
+            NumberOfSteps = 10000   #If including first 10% of data values
+    else:
+        steps = 1
+        NumberOfSteps = 900
+        #NumberOfSteps = 900
+    #NumberOfSteps=10000
+    NumberOfSteps = 1000
+
+    print "Number of steps = ", NumberOfSteps
+    # Create list containing each temperature in a given file
+    Temps = data[:, 1]
+    if NumberOfValues > NumberOfSteps:  #If several temperatures in a file
+#        print Temps[NumberOfSteps*(NumberOfTemp)-1], Temps[NumberOfSteps*(NumberOfTemp-1)-1]
+        temp_step = Temps[NumberOfSteps*(NumberOfTemp)-1] - Temps[NumberOfSteps*(NumberOfTemp-1)-1]
+        print "temp_step = %.3f" %temp_step
+    else:
+        temp_step = 1   #If only one temperature in a file
+    if NumberOfTemp > 2:
+        Temperatures = np.arange(Temps[0], Temps[-1], temp_step)
+    elif NumberOfTemp == 2:
+        Temperatures = [1.0, 2.4]
+
+    #Obtain values from each coulumn in file
+    NumberOfTemp = len(Temperatures)
+    MCs = data[:, 0]
+    Energy = data[:, 2]
+    Specific_heat = data[:, 3]
+    MagneticMoment = data[:, 4]
+    Susceptibility = data[:, 5]
+    abs_MagneticMoment = data[:, 6]
+    varE = np.zeros(NumberOfSteps)
+    #acc_configs = data[:, -1]
 
 
-MCs = data[:, 0]
-Temps = data[:, 1]
-#print Temps[NumberOfMc*1-NumberOfMc]
-#Temperatures = np.arange(Temps[0], Temps[-1], temp_step)
-Temperatures = np.arange(Temps[0], Temps[-1], 0.2)
-Energy = data[:, 2]
-Specific_heat = data[:, 3]
-MagneticMoment = data[:, 4]
-Susceptibility = data[:, 5]
-abs_MagneticMoment = data[:, 6]
-if data.shape[1] >= 7:
-    acc_configs = data[:, -1]
+    #Plot for different variables as a function Monte Carlo cycles
+    for l in xrange(0, NumberOfTemp):
+        #print "MCs[%.3f] = %.3f" %(NumberOfSteps*(l)+l, MCs[NumberOfSteps*(l)+l])
+        #print "T = %3f" %Temps[NumberOfSteps*(l)+l]
+        #print "MCs[%.3f] = %.3f" %(NumberOfSteps*(l+1)+l, MCs[NumberOfSteps*(l+1)+l])
+        #print "T = %3f \n" %Temps[NumberOfSteps*(l+1)+l]
+        #Plot.PlotMCVarE(MCs[NumberOfSteps*(l)+l:NumberOfSteps*(l+1)+l], Specific_heat[NumberOfSteps*(l)+l:NumberOfSteps*(l+1)+l]*Temperatures[l]**2, Temps[(NumberOfSteps+1)*(l)], filename)
+        #Plot.PlotProbE(Energy[NumberOfSteps*(l)+l:NumberOfSteps*(l+1)+l], Temperatures[l], filename)
+        #Plot.PlotProbE(Energy[NumberOfSteps*(l):NumberOfSteps*(l+1)-l], Temps[(NumberOfSteps+1)*(l)], filename)
+        #Plot.PlotMCEnergy(MCs[NumberOfSteps*(l)+l:NumberOfSteps*(l+1)+l], Energy[NumberOfSteps*(l)+l:NumberOfSteps*(l+1)+l], Temperatures[l], NumberOfSpins, filename)
+        #Plot.PlotMCCv(MCs[NumberOfSteps*(l)+l:NumberOfSteps*(l+1)+l], Specific_heat[NumberOfSteps*(l)+l:NumberOfSteps*(l+1)+l], Temps[(NumberOfSteps+1)*(l)], NumberOfSpins, filename)
+        Plot.PlotMCMag(MCs[NumberOfSteps*(l)+l:NumberOfSteps*(l+1)+l], abs_MagneticMoment[NumberOfSteps*(l)+l:NumberOfSteps*(l+1)+l], Temps[(NumberOfSteps+1)*(l)], NumberOfSpins, filename)
     #for l in xrange(1, NumberOfTemp+1):
         #Plot.PlotMCAcc_configs(MCs[NumberOfMc*(l-1):NumberOfMc*(l)], acc_configs[NumberOfMc*(l-1):NumberOfMc*(l)], filename)
         #print NumberOfMc*(l-1), NumberOfMc*(l)
-"""
-for l in xrange(1, NumberOfTemp+1):
-    Plot.PlotMCEnergy(MCs[NumberOfMc*(l-1):NumberOfMc*(l)], Energy[NumberOfMc*(l-1):NumberOfMc*(l)], Temps[(NumberOfMc+1)*(l-1)], NumberOfMc, filename)
-    Plot.PlotMCCv(MCs[NumberOfMc*(l-1):NumberOfMc*(l)], Specific_heat[NumberOfMc*(l-1):NumberOfMc*(l)], Temps[(NumberOfMc+1)*(l-1)], filename)
-    Plot.PlotMCMag(MCs[NumberOfMc*(l-1):NumberOfMc*(l)], abs_MagneticMoment[NumberOfMc*(l-1):NumberOfMc*(l)], Temps[(NumberOfMc+1)*(l-1)], filename)
-"""
-"""
-for l in xrange(10, NumberOfTemp+1):
 
-    print "MCs[%.3f] = %.3f" %(NumberOfSteps*(l-1), MCs[NumberOfSteps*(l-1)])
-    print "T = %3f" %Temps[NumberOfSteps*(l-1)]
-    print "MCs[%.3f] = %.3f" %(NumberOfSteps*(l), MCs[NumberOfSteps*(l)-1])
-    print "T = %3f" %Temps[NumberOfSteps*(l)-1]0
 
-    Plot.PlotMCEnergy(MCs[NumberOfSteps*(l-1):NumberOfSteps*(l)-1], Energy[NumberOfSteps*(l-1):NumberOfSteps*(l)-1], Temps[(NumberOfSteps+1)*(l-1)], NumberOfMc, filename)
-    Plot.PlotMCCv(MCs[NumberOfSteps*(l-1):NumberOfSteps*(l)], Specific_heat[NumberOfSteps*(l-1):NumberOfSteps*(l)], Temps[(NumberOfMc+1)*(l-1)], filename)
-    Plot.PlotMCMag(MCs[NumberOfSteps*(l-1):NumberOfSteps*(l)], abs_MagneticMoment[NumberOfSteps*(l-1):NumberOfSteps*(l)], Temps[(NumberOfMc+1)*(l-1)], filename)
-"""
-"""
-for l in xrange(1, NumberOfTemp+1):
-    Plot.PlotMCEnergy(MCs[NumberOfMc*(l-1):NumberOfMc*(l)], Energy[NumberOfMc*(l-1):NumberOfMc*(l)], Temps[(NumberOfMc+1)*(l-1)], NumberOfMc, filename)
-    Plot.PlotMCCv(MCs[NumberOfMc*(l-1):NumberOfMc*(l)], Specific_heat[NumberOfMc*(l-1):NumberOfMc*(l)], Temps[(NumberOfMc+1)*(l-1)], filename)
-    Plot.PlotMCMag(MCs[NumberOfMc*(l-1):NumberOfMc*(l)], abs_MagneticMoment[NumberOfMc*(l-1):NumberOfMc*(l)], Temps[(NumberOfMc+1)*(l-1)], filename)
-"""
+    # Plot for different variables as a function of temperature
+    accepted_temp = np.zeros(NumberOfTemp)
+    Cv_mean = np.zeros(NumberOfTemp)
+    Mabs_mean = np.zeros(NumberOfTemp)
+    E_mean = np.zeros(NumberOfTemp)
+    Chi_mean = np.zeros(NumberOfTemp)
+    varE_mean = np.zeros(NumberOfTemp)
+    M_mean = np.zeros(NumberOfTemp)
 
-#for every total number of Monte Carlo cycles for each temperature, extract the final Cv value and insert in Cv1
-Cv_mean = np.zeros(NumberOfTemp)
-M_mean = np.zeros(NumberOfTemp)
-accepted = np.zeros(NumberOfTemp)
-E_mean = np.zeros(NumberOfTemp)
+    Tc_possible = []
+    for i in xrange(NumberOfTemp):
+        Cv_mean[i] = sum(Specific_heat[i*NumberOfSteps:(i+1)*NumberOfSteps])/NumberOfSteps
+        Mabs_mean[i] = sum(abs_MagneticMoment[i*NumberOfSteps:(i+1)*NumberOfSteps])/NumberOfSteps
+        E_mean[i] = sum(Energy[i*NumberOfSteps:(i+1)*NumberOfSteps])/NumberOfSteps
+        Chi_mean[i] = sum(Susceptibility[i*NumberOfSteps:(i+1)*NumberOfSteps])/NumberOfSteps
+        M_mean[i] = sum(MagneticMoment[i*NumberOfSteps:(i+1)*NumberOfSteps])/NumberOfSteps
+        varE_mean[i] = Cv_mean[i]*Temperatures[i]**2
+    #Plot.PlotTempAcc(Temperatures, accepted_ordered, NumberOfSpins, filename)
+    #Plot.PlotTempAcc(Temperatures, accepted_random, NumberOfSpins, filename)
+    #Plot.PlotTempEnergy(Temperatures, E_mean, NumberOfSpins, filename)
+    Plot.PlotTempCv(Temperatures, Cv_mean, NumberOfSpins, filename)
+    #Plot.PlotTempMag(Temperatures, Mabs_mean, NumberOfSpins, filename)
+    #Plot.PlotTempMag(Temperatures, M_mean, NumberOfSpins, filename)
+    #Plot.PlotTempVarE(Temperatures, varE_mean)
+    #Plot.PlotTempChi(Temperatures, Chi_mean, NumberOfSpins, filename)
 
-for i in xrange(NumberOfTemp):
-    Cv_mean[i] = sum(Specific_heat[i*NumberOfSteps:(i+1)*NumberOfSteps])/NumberOfSteps
-    M_mean[i] = sum(abs_MagneticMoment[i*NumberOfSteps:(i+1)*NumberOfSteps])/NumberOfSteps
-    E_mean[i] = sum(Energy[i*NumberOfSteps:(i+1)*NumberOfSteps])/NumberOfSteps
-#    accepted[]
-#    print Cv[i*NumberOfMc]
-#PlotCvTemp(Temperatures, Cv)
-Plot.PlotTempEnergy(Temperatures, E_mean, NumberOfSpins, filename)
-Plot.PlotTempCv(Temperatures, Cv_mean, NumberOfSpins, filename)
-Plot.PlotTempMag(Temperatures, M_mean, NumberOfSpins, filename)
-#
+    # Get possible critical temperatures for each lattice if |M| < 0
+
+
+    for k in xrange(NumberOfValues):
+        if MagneticMoment[k] < 0:
+            Tc_possible.append(Temps[k])
+
+    # Append the lowest critical temperature for each lattice to list Tc
+    if len(Tc_possible) > 0:
+        Tc.append(Tc_possible[0])
+        #L.append(NumberOfSpins)
+        L_inv.append(1/float(NumberOfSpins**2))
+
+    print "# Tc_possible = ", len(Tc_possible)
+#print "Tc_possible = ", Tc_possible
+#print "T_c =", Tc
+
+plt.legend(loc='best', fontsize=12)
+#plt.savefig('Plots/Plots_e/Temp_chi_multiple_lattices')
+plt.show()
+# Plot critical temperature for each lattice
+#slope, intercept, r_value, p_value, std_err = stats.linregress(L_inv, Tc)
+#print slope
+#print "T_c = %.3f" %intercept
+#a = slope*L + intercept
+#print a
+"""
+plt.plot(L_inv, slope*np.array(L_inv) + intercept, 'r-')
+plt.plot(L_inv, Tc, 'b.')
+plt.title(r'Critical temperature $T_c$')
+plt.legend([r'$T_C = %g$' %intercept])
+plt.xlabel(r'$1/L^2$')
+plt.ylabel(r'$T_C [J/k_B]$')
+plt.savefig('Plots/Plots_f/Critical_temperature')
+plt.show()
+"""
+#If NumberOfSteps=10000:
+"""
+for l in xrange(0, NumberOfTemp):
+    print "MCs[%.3f] = %.3f" %(NumberOfSteps*(l)-l, MCs[NumberOfSteps*(l)-l])
+    print "T = %3f" %Temps[NumberOfSteps*(l)-l]
+    print "MCs[%.3f] = %.3f" %(NumberOfSteps*(l+1)-l, MCs[NumberOfSteps*(l+1)-l])
+    print "T = %3f \n" %Temps[NumberOfSteps*(l+1)-l]
+
+    #Plot.PlotMCVarE(MCs[NumberOfSteps*(l)+l:NumberOfSteps*(l+1)+l], Specific_heat[NumberOfSteps*(l)+l:NumberOfSteps*(l+1)+l], Temps[(NumberOfSteps+1)*(l)])
+    #Plot.PlotProbE(Energy[NumberOfSteps*(l)+l:NumberOfSteps*(l+1)+l], Temperatures[l], filename)
+    #Plot.PlotMCEnergy(MCs[NumberOfSteps*(l)+l:NumberOfSteps*(l+1)+l], Energy[NumberOfSteps*(l)+l:NumberOfSteps*(l+1)+l], Temperatures[l], NumberOfSpins, filename)
+    #Plot.PlotMCCv(MCs[NumberOfSteps*(l):NumberOfSteps*(l+1)-1], Specific_heat[NumberOfSteps*(l):NumberOfSteps*(l+1)-1], Temps[(NumberOfSteps+1)*(l)], NumberOfSpins, filename)
+    #Plot.PlotMCMag(MCs[NumberOfSteps*(l):NumberOfSteps*(l+1)-1], abs_MagneticMoment[NumberOfSteps*(l):NumberOfSteps*(l+1)-1], Temps[(NumberOfSteps+1)*(l)], NumberOfSpins, filename)
+"""
--- Project_4/PlotFunctions.py
+++ Project_4/PlotFunctions.py
@@ -8,50 +8,65 @@ def analytic(temps, nSpin):
     nTemp = len(temps)
     energy_analytic = np.zeros(nTemp)
     Cv_analytic = np.zeros(nTemp)
-    M_analytic = np.zeros(nTemp)
+    Mabs_analytic = np.zeros(nTemp)
+    chi_analytic = np.zeros(nTemp)
     for i in xrange(nTemp):
         beta = 1/float(k_B*temps[i])
         beta2 = 1/float(k_B*temps[i]**2)
         Z = 3+np.cosh(8*beta*J)
         energy_analytic[i] = -8*J*np.sinh(8*beta*J)/float(Z)/float(nSpin**2)     #mean energies
         Cv_analytic[i] = beta2*((64*J**2*np.cosh(8*beta*J)/float(Z)) - (64*np.sinh(8*beta*J)**2)/float(Z**2))/float(nSpin*nSpin) #mean Cv
-        M_analytic[i] = (2*np.exp(8*beta*J) + 4)/float(Z)/float(nSpin*nSpin)
-    return energy_analytic, Cv_analytic, M_analytic
+        Mabs_analytic[i] = (2*np.exp(8*beta*J) + 4)/float(Z)/float(nSpin*nSpin)
+        chi_analytic[i] = beta*((8 * np.exp(8*beta*J) + 8)/float(Z) - (4 * Mabs_analytic[i])**2) / 4
+    return energy_analytic, Cv_analytic, Mabs_analytic, chi_analytic
 
 def PlotTempEnergy(temps, energy, nSpin, filename):
-    plt.plot(temps, energy, 'r.')
-    plt.plot(temps, analytic(temps, nSpin)[0], 'b-')
+    plt.plot(temps, energy, '.', label=r'$%d\times%d$'%(nSpin, nSpin))
+#    plt.plot(temps, analytic(temps, nSpin)[0], 'b-')
     plt.xlabel(r'$k_BT$')
     plt.ylabel(r'E/J')
-    plt.legend([r'$%d\times%d$ Numeric'%(nSpin, nSpin), r'$%d\times%d$ Analytic'%(nSpin, nSpin)], loc='best', fontsize=12)
+    #plt.legend([r'$%d\times%d$ Numeric'%(nSpin, nSpin), r'$%d\times%d$ Analytic'%(nSpin, nSpin)], loc='best', fontsize=12)
     plt.title(r'Energy $\langle E \rangle$')
     plt.grid('on')
-#    plt.savefig(filename+'T_'+str(T)+'MC_E.png')
-    plt.show()
+    #plt.savefig(filename+'TempE.png')
+    #plt.show()
 
+listCv = []
 def PlotTempCv(temps, Cv, nSpin, filename):
-    plt.plot(temps, Cv, 'r.')
-    plt.plot(temps, analytic(temps, nSpin)[1], 'b-')
+    plt.plot(temps, Cv, '.', label=r'$%d\times%d$'%(nSpin, nSpin))
+#    plt.plot(temps, analytic(temps, nSpin)[1], 'b-')
     plt.xlabel(r'$k_BT$')
     plt.ylabel(r'$C_V/Jk_B$')
-    plt.legend([r'$%d\times%d$ Numeric'%(nSpin, nSpin), r'$%d\times%d$ Analytic'%(nSpin, nSpin)], loc='best', fontsize=12)
+#    plt.legend([r'$%d\times%d$ Numeric'%(nSpin, nSpin)], loc='best', fontsize=12)
     plt.title(r'Heat capacity $C_V$')
     #plt.axis('equal')
     plt.grid('on')
-#    plt.savefig(filename+'T_'+str(T)+'MC_Cv.png')
-    plt.show()
+    #plt.savefig(filename+'TempCv.png')
+    #plt.show()
 
 def PlotTempMag(temps, Mag, nSpin, filename):
-    plt.plot(temps, Mag, 'r.')
-    plt.plot(temps, analytic(temps, nSpin)[2], 'b-')
+    ax = plt.plot(temps, Mag, '.', label=r'$%d\times%d$'%(nSpin, nSpin))
+#    plt.plot(temps, analytic(temps, nSpin)[2], 'b-')
     plt.xlabel(r'$k_BT$')
     plt.ylabel(r'$\langle |M| \rangle$')
-    plt.legend([r'$%d\times%d$ Numeric'%(nSpin, nSpin), r'$%d\times%d$ Analytic'%(nSpin, nSpin)], loc='best', fontsize=12)
-    plt.title(r'Magnetic Moment $\langle |M| \rangle$')
-#    plt.savefig(filename+'T_'+str(T)+'MC_Mag.png')
+#    plt.legend([r'$%d\times%d$ Numeric'%(nSpin, nSpin), r'$%d\times%d$ Analytic'%(nSpin, nSpin)], loc='best', fontsize=12)
+    plt.title(r'$\langle |M| \rangle$')
     #plt.axis('equal')
     plt.grid('on')
-    plt.show()
+    #plt.savefig('Plots/Plots_e/' + filename + 'TempMag.png')
+    #plt.show()
+
+def PlotTempChi(temps, chi, nSpin, filename):
+    plt.plot(temps, chi, '.', label=r'$%d\times%d$'%(nSpin, nSpin))
+#    plt.plot(temps, analytic(temps, nSpin)[3], 'b-')
+    plt.xlabel(r'$k_BT$')
+    plt.ylabel(r'$\chi$')
+    #plt.legend([r'$%d\times%d$ Numeric'%(nSpin, nSpin), r'$%d\times%d$ Analytic'%(nSpin, nSpin)], loc='best', fontsize=12)
+    plt.title(r'Susceptibility $\chi$')
+    #plt.axis('equal')
+    plt.grid('on')
+    #plt.savefig(filename+'TempChi.png')
+    #plt.show()
 
 def PlotMCAcc_configs(MC, acc, filename):
     plt.plot(MC, acc, 'g.')
@@ -63,8 +78,68 @@ def PlotMCAcc_configs(MC, acc, filename):
     plt.grid('on')
     plt.show()
 
+def PlotTempAcc(T, acc, nSpin, filename):
+    plt.plot(T, acc, 'r-')
+    plt.xlabel('Temperatures')
+    plt.ylabel('Accepted states')
+#    plt.legend([r'T = %.3f' %T], loc='best', fontsize=12)
+    plt.title(r'Energy $\langle E \rangle$')
+    plt.grid('on')
+#    plt.savefig(filename+'T_'+str(T)+'MC_E.png')
+    plt.show()
 
 
+def PlotMCEnergy(MC, energy, T, nSpin, filename):
+    plt.plot(MC, energy, 'r-')
+    plt.xlabel('Monte Carlo cycles')
+    plt.ylabel(r'E/J')
+    plt.legend([r'T = %.3f' %T], loc='best', fontsize=12)
+    plt.title(r'Energy $\langle E \rangle$')
+    plt.grid('on')
+    plt.savefig('Plots/Plots_d/'+filename+'T_'+str(T)+'MC_E.png')
+    plt.show()
+
+def PlotMCCv(MC, Cv, T, nSpin, filename):
+    plt.plot(MC, Cv, 'r-')
+    plt.xlabel('Monte Carlo cycles')
+    plt.ylabel(r'$C_V$')
+    plt.legend([r'T = %.3f' %T], loc='best', fontsize=12)
+    plt.title(r'Specific heat C_V')
+    plt.grid('on')
+    #plt.savefig(filename+'T_'+str(T)+'MC_Cv.png')
+    plt.show()
+
+def PlotMCMag(MC, mag, T, nSpin, filename):
+    plt.plot(MC, mag, 'r-')
+    plt.xlabel(r'$k_BT$')
+    plt.ylabel(r'$\langle |M| \rangle$')
+    plt.legend([r'T = %.3f' %T], loc='best', fontsize=12)
+#    plt.legend([r'$%d\times%d$ Numeric'%(nSpin, nSpin), r'$%d\times%d$ Analytic'%(nSpin, nSpin)], loc='best', fontsize=12)
+    plt.title(r'Magnetic Moment $\langle |M| \rangle$')
+    plt.grid('on')
+    #plt.savefig(filename+'T_'+str(T)+'MC_Mag.png')
+    plt.show()
+
+def PlotProbE(energy, temps, filename):
+    #c = filename.split('_')[-1]
+    #config=c.split('.')[0]
+    plt.figure('Probability histogram')
+    plt.hist(energy, bins=100, facecolor='r')
+    plt.title('Probability distribution of Energy at T = %g' %(temps))
+    plt.xlabel('Energies, [J]')#, size=14)
+    plt.ylabel('Probability')#, size=14)
+    plt.grid('on')
+    plt.savefig('Plots/Plots_d/' + filename + 'T_%s'%temps +'probE.png')
+    plt.show()
+
+def PlotMCVarE(MCs, varE, T, filename):
+    plt.plot(MCs, varE, 'r-', label='T=%g'%T)
+    plt.xlabel('Monte Carlo')
+    plt.ylabel(r'$\sigma_E^2$')
+    plt.title(r'T = %g'%T)
+    plt.grid('on')
+    plt.savefig('Plots/Plots_d/' + filename + 'T_'+str(T)+'MC_varE.png')
+    plt.show()
 
 
 #from analytic import Printing
